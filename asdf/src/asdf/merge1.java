345292c599bfebf2897c6e2cc00187d8cf8b2ad0
ASUS <ASUS@DESKTOP-A9P3PNI>
2023-03-19 14:19:37 +0900

    merge 연습 #2

---
+++ asdf/src/asdf/merge1.java
@@ -0,0 +1,13 @@
+package asdf;
+
+public class merge1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		int y = 2;
+		
+		System.out.println("x + y = " + x + y);
+	}
+
+}
