ebbc402ca9f190f30032940e746913c995285b9f
JaeYong.Seo <wodyd3215@naver.com>
2023-04-04 12:36:36 +0900

    github commit #1

--- src/git.java
+++ src/git.java
@@ -3,7 +3,7 @@ public class git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Github 연습입니다.");
+		System.out.println("Github commit 연습입니다.");
 	}
 
 }
